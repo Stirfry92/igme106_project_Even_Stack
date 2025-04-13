63799680ec2604decee30e30737f83f6856b9f83
zda9250 <zda9250@rit.edu>
2025-04-13 16:21:02 -0400

    Set game to fullscreen 1920x1080

--- ASTRA/Game1.cs
+++ ASTRA/Game1.cs
@@ -19,7 +19,11 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        _graphics.PreferredBackBufferWidth = 1920;
+        _graphics.PreferredBackBufferHeight = 1080;
+        _graphics.IsFullScreen = true;
 
+        _graphics.ApplyChanges();
         base.Initialize();
     }
 
@@ -42,7 +46,7 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
 
         // TODO: Add your drawing code here
 
